18e1c5972125a59ea84e07254532fb755389a5c3
ishaq60 <ishaqahammad60@gmail.com>
2024-08-26 01:47:19 +0600

    Project Done

--- src/Component/PrivateRoute/PrivateRoute.jsx
+++ src/Component/PrivateRoute/PrivateRoute.jsx
@@ -6,9 +6,9 @@ import { Navigate, useLocation } from 'react-router-dom';
 const PrivateRoute = ({children}) => {
     const {user,loading}=UseAuth()
     const location=useLocation()
-    if(loading){
-        return <h1 className='text-4xl'>Loading</h1>
-       }
+    // if(loading){
+    //     return <h1 className='text-4xl'>Loading</h1>
+    //    }
     if(!user){
         return <Navigate to='/login' state={location?.pathname||'/'}></Navigate>
     }
